ab8f14b2c8b4c57bbcb69d3297eac86f4d9c8032
Trung Nguyen <K215480106067@tnut.edu.vn>
2025-03-04 12:55:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/app2/ThongTIN.java
@@ -0,0 +1,29 @@
+package com.example.app2;
+
+public class ThongTIN {
+    private String id,title,body;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
